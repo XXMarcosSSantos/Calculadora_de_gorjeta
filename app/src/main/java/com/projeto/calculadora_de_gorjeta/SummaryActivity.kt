87d3477856a60a992d87cc60eb503776ae575a2a
marco <marcosxsantosbkj@gmail.com>
2024-12-20 13:49:03 -0300

    tela final com o resultado

--- app/src/main/java/com/projeto/calculadora_de_gorjeta/SummaryActivity.kt
+++ app/src/main/java/com/projeto/calculadora_de_gorjeta/SummaryActivity.kt
@@ -5,17 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.projeto.calculadora_de_gorjeta.databinding.ActivityMainBinding
+import com.projeto.calculadora_de_gorjeta.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySummaryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_summary)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val totalTable = intent.getFloatExtra("totalTable", 0.0f)
         val numPeople = intent.getIntExtra("numPeople", 0)
         val percentage = intent.getIntExtra("percentage", 0)
         val totalAmount = intent.getFloatExtra("totalAmount", 0.0f)
 
-        println("resultado " + totalAmount)
+        binding.tvPercentage.text = percentage.toString()
+        binding.tvTotalAmount.text = totalAmount.toString()
+        binding.tvTotalTable.text = totalTable.toString()
+        binding.tvNumPeopleTable.text = numPeople.toString()
+
+        binding.btnRefreash.setOnClickListener {
+            finish()
+        }
     }
 }
